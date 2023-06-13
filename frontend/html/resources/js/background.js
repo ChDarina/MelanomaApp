14c347313ae5e86ea317d4ae041ef3087744a733
ChDarina <dvchupina@edu.hse.ru>
2023-06-14 00:31:23 +0500

    design first ed changes

---
+++ frontend/html/resources/js/background.js
@@ -0,0 +1,15 @@
+const background = `
+<ul class="circles">
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+</ul>
+`
+document.body.insertAdjacentHTML("afterbegin", background)
--- frontend/html/resources/js/navigation.js
+++ frontend/html/resources/js/navigation.js
@@ -1,15 +1,14 @@
 const isDoctor = sessionStorage.getItem("isDoctor");
 
 let navigation = `
-<header class="p-3 mb-3 border-bottom">
+<header class="p-3 mb-3">
 <div class="container">
-    <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+    <div class="d-flex flex-wrap justify-content-around justify-content-lg-start">
         <h3>
             <a href="http://0.0.0.0:3001/index"
                 class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none"><b>SkinCancerApp</b></a>
         </h3>
-
-        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0" id="functions">
             <li><a href="http://0.0.0.0:3001/articles" class="nav-link px-2 link-dark">Статьи</a></li>
             <li><a href="http://0.0.0.0:3001/questions" class="nav-link px-2 link-dark">Вопросы</a></li>
             `
@@ -22,7 +21,7 @@ navigation += `
         `
 
 if (window.location.pathname == "/articles" || window.location.pathname == "/questions"
-|| window.location.pathname == "/doctors" || window.location.pathname == "/appointments")
+    || window.location.pathname == "/doctors" || window.location.pathname == "/appointments")
     navigation += `
         <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
             <input type="search" class="form-control" placeholder="Поиск..." aria-label="Search">
@@ -32,8 +31,8 @@ if (window.location.pathname == "/articles" || window.location.pathname == "/que
 navigation += `
         <div class="dropdown text-end">
             <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" id="dropdownUser1"
-                data-bs-toggle="dropdown" aria-expanded="false">
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                data-bs-toggle="dropdown" aria-expanded="false" style="color: rgb(255, 255, 255);">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" style="color: rgb(255, 255, 255);" class="bi bi-person-circle" viewBox="0 0 16 16">
                     <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
                     <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
                   </svg>
@@ -41,8 +40,11 @@ navigation += `
             <ul class="dropdown-menu text-small" aria-labelledby="dropdownUser1">
                 <li><a class="dropdown-item" href="http://0.0.0.0:3001/profile">Профиль</a></li>
                 <li><a class="dropdown-item" href="#">Настройки</a></li>
-                <li><a class="dropdown-item" href="http://0.0.0.0:3001/appointments">Мои приёмы</a></li>
-                <li>
+                <li><a class="dropdown-item" href="http://0.0.0.0:3001/appointments">Мои приёмы</a></li>`;
+if (isDoctor === 'false') navigation += `<li><a class="dropdown-item" href="http://0.0.0.0:3001/profile">Мои вопросы</a></li>`;
+else navigation += `<li><a class="dropdown-item" href="http://0.0.0.0:3001/profile">Мои статьи</a></li>
+                    <li><a class="dropdown-item" href="http://0.0.0.0:3001/profile">Мои ответы</a></li>`;
+navigation += `<li>
                     <hr class="dropdown-divider">
                 </li>
                 <li><button class="dropdown-item" onclick="logout()">Выйти</button></li>
